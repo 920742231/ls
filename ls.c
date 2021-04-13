083cef87dd1afd3c6fb5767d6edbaa3ebb770115
TomAndJerry <920742231@qq.com>
2021-04-13 15:43:18 +0800

    ls -lR

--- ls.c
+++ ls.c
@@ -12,19 +12,21 @@
 #include<dirent.h>
 #include<sys/types.h>
 
+#ifndef MALLOC
 #define MALLOC(value,type,size) {\
     value = (type)malloc(size);\
     assert(value);}
-
-void do_ls(char * fname,int options);
-
-static inline void err_exit(char *,int);
-
-void show_st(char *,struct stat *);
-
+#endif
 
 #define LONG_MOD(mode) (mode |= 0x02)
 #define RECU_MOD(mode) (mode |= 0x04)
+
+#ifndef PUSAGE
+#define PUSAGE printf(\
+    "Usage: ls [-l]/[-R]/[-lR]/ <file list>\n")
+#endif
+
+void do_ls(char * fname,int options);
 
 int main(int argc,char ** argv) {
 
@@ -32,43 +34,59 @@ int main(int argc,char ** argv) {
     char opt;
     char ** args;
     
-    mode = 1;
-
     /*
-     *  Explain commamd options by user parameters.
+     *  Set mode 0001 as not options,print file 
+     *  state on simple mode(just file name).
      */
-#define PUSAGE printf(\
-    "Usage: ls [-l]/[-R]/[-lR]/ <file list>\n")
-
+    mode = 1;
+    /*  -------------------------------------------
+     *  Explain commamd options by user parameters.
+     *  And add options to value 'mode' to record.
+     *  
+     *  LONG_MODE----------------------------------
+     *  '-l' in options,means print file message on
+     *  long mode(use file states in struct stat).
+     *  Set mode |= 0010 as long mode.
+     *  
+     *  RECU_MODE----------------------------------
+     *  '-R' in options,means print file message on
+     *  recu mode(Recuersivly prirnt the direntory 
+     *  tree).
+     *  Set mode |= 0100 as recu mode.
+     *  
+     *  PUSAGE-------------------------------------
+     *  Wrong optons,show tips to user.
+     * 
+     *  And option '-l' and '-R' are legal to use 
+     *  together.
+     */
     while((opt = getopt(argc,argv,"lR")) \
-    != -1){
-    switch(opt) {
-    case 'l':   LONG_MOD(mode);
-                break;
-    case 'R':   RECU_MOD(mode);
-                break;
-    default:    PUSAGE;
-                exit(0);
+        != -1){
+        switch(opt) {
+        case 'l':   LONG_MOD(mode);
+                    break;
+        case 'R':   RECU_MOD(mode);
+                    break;  
+        default:    PUSAGE;
+                    exit(0);
         }
     }
-    
-    /*
-     *  Create files list to do ls.
-     */
+
+    /*  Create files list.  */
     if(optind == argc) {
+    /*  No other args as file name,use default file "." */
         MALLOC(args,char **,sizeof(char **) * 2);
         MALLOC(args[0],char *,sizeof(char) * 2);
         strcpy(args[0],".");
         args[1] = NULL;
-    /*  No other args as file name,use default file "." */
     }
     else {
+    /*  Files' names are indicated by user parameters. */
         MALLOC(args,char **,
             sizeof(char *) * (argc - optind + 1));
         for(int i=optind,j = 0;i < argc;++i,++j)
             args[j] = argv[i];
         args[argc - optind] = NULL;
-    /*  Files' names are indicated by user parameters. */
     }
 
     while(*args) do_ls(*(args++),mode);
@@ -89,6 +107,10 @@ LONGRECU : ls -lR / ls -l -R */
 
 #define FNMELEN     512
 
+static inline void err_exit(char *,int);
+
+void show_st(char *,struct stat *);
+
 /*
  * Show file state on long mode or normal mode.
  */
@@ -97,9 +119,9 @@ static void __norm_long(struct stat * st,
     
     int fd;
     DIR * dir;
-    char name[FNMELEN];
     struct stat _st;
     struct dirent * dt;
+    char name[FNMELEN];
 
     /*
      *  Not a directory,show its message.
@@ -109,25 +131,28 @@ static void __norm_long(struct stat * st,
         else if(mode == LONGMODE) show_st(fname,st);
         return;
     }
-
     /*
      *  Else,open this driectory and show its child
      *  files's message.
      */
-    if(!(dir = opendir(fname)))
+    if(!(dir = opendir(fname))) 
         err_exit("opendir",-errno);
 
     printf("%s:\n",fname);
 
     while((dt = readdir(dir))) {
-    if(mode == NORMMODE) printf("%s  ",dt->d_name);
+    if(mode == NORMMODE) 
+        printf("%s  ",dt->d_name);
     else if(mode == LONGMODE) {
         sprintf(name,"%s/%s",fname,dt->d_name);
+        
         if((fd = open(name,O_RDONLY)) < 0)
             err_exit("open",-errno);
         if(fstat(fd,&_st) < 0) 
             err_exit("fstat",-errno);
+        
         show_st(dt->d_name,&_st);
+        
         close(fd);
         }
     }
@@ -143,9 +168,9 @@ static void __recu_ls(struct stat * st,
 
     int fd;
     DIR * dir;
-    char name[FNMELEN];
     struct stat _st;
     struct dirent * dt;
+    char name[FNMELEN];
 
     /*
      *  Show current file's message.
@@ -164,7 +189,9 @@ static void __recu_ls(struct stat * st,
         if(strcmp(".",dt->d_name) == 0 \
         || strcmp("..",dt->d_name) == 0)
             continue;
+        
         sprintf(name,"%s/%s",fname,dt->d_name);
+        
         if((fd = open(name,O_RDONLY)) < 0)
             err_exit("open",-errno);
         if(fstat(fd,&_st) < 0) 
@@ -172,8 +199,7 @@ static void __recu_ls(struct stat * st,
         /*
          *  Not a directory,don't need showing states.
          */
-        if(!S_ISDIR(_st.st_mode))
-			goto cont;
+        if(!S_ISDIR(_st.st_mode)) goto cont;
 
         /*
          *  Else.
@@ -220,8 +246,6 @@ void do_ls(char * name,int options) {
     close(fd);
 }
 
-#define FMOD_TRANS(mode) (mode & 0x07)
-
 /*
 File modes:
 EXEC - "--x"(001)
@@ -239,6 +263,8 @@ RWAX - "rwx"(111)
 #define RADX    5
 #define RADW    6
 #define RWEX    7
+
+#define FMOD_TRANS(mode) (mode & 0x07)
 
 static void __explain_mod(int mode,char * str) {
 
@@ -270,14 +296,10 @@ static char * __get_mode_str(int mode) {
      *  Get file type.
      */
     struct stat st;
-    if(S_ISDIR(mode))
-        mode_str[0] = 'd';
-    else if(S_ISCHR(mode))
-        mode_str[0] = 'c';
-    else if(S_ISBLK(mode))
-        mode_str[0] = 'b';
-    else 
-        mode_str[0] = '-';
+    if(S_ISDIR(mode)) mode_str[0] = 'd';
+    else if(S_ISCHR(mode)) mode_str[0] = 'c';
+    else if(S_ISBLK(mode)) mode_str[0] = 'b';
+    else mode_str[0] = '-';
 
     pos = 1;
     
@@ -313,11 +335,13 @@ static void __show_st(char * fname,struct stat * st) {
     char * usr_name;
     char * grp_name;
 
-    mode_str = __get_mode_str(st->st_mode);
-    time_str = ctime(&st->st_mtim.tv_sec);
-    time_str[strlen(time_str) - 1] = 0;
-    usr_name = getuname(st->st_uid);
-    grp_name = getgname(st->st_gid);
+    mode_str = __get_mode_str(st->st_mode); //mode string
+
+    time_str = ctime(&st->st_mtim.tv_sec);  //time string
+    time_str[strlen(time_str) - 1] = 0;     //delete '/n'
+
+    usr_name = getuname(st->st_uid);        //user name
+    grp_name = getgname(st->st_gid);        //group name
 
     __SHOW_ST(mode_str,st->st_nlink,usr_name,grp_name,
         st->st_size,time_str,fname);
